0b425b214598a37115f38bce48d547b801f56d42
Tim Ahrentløv <tim@ahrentlov.dk>
2022-06-20 12:05:08 +0200

    Tweaks to make it very basic, run using VSC and strip of CSS

--- src/Taskmote.Blazor.WASM/Program.cs
+++ src/Taskmote.Blazor.WASM/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TaskmoteWA;
+using Taskmote.Blazor.WASM;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
